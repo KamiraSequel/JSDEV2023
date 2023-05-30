6451fae4bdcb72aaea384cbc49611a18778b5eb8
Frontend JavaScript <frontendjavascript@C3-03.local>
2023-05-30 21:00:28 +0300

    corcosoco

--- angularCrud/frontend/src/app/app.module.ts
+++ angularCrud/frontend/src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProfilComponent } from './profil/profil.component';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from './api.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AdminComponent
+    AdminComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    ApiService,
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
